6eb9347d9000110e57267b09cfd9712667a6d689
tinacho12 <146214538+tinacho12@users.noreply.github.com>
2023-10-17 22:55:38 +0000

    Added homepage content

--- app.rb
+++ app.rb
@@ -1,9 +1,22 @@
 require "sinatra"
 require "sinatra/reloader"
 
+#Homepage
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
   "
+end
+
+#Play Rock
+get("/rock") do
+end
+
+#Play Paper
+get("/paper") do
+end
+
+#Play Scissors
+get("/scissors") do
 end
